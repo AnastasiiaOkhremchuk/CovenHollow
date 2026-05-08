764718b3f7e68eac1e474f32fbed6949b884379a
Anastasiia Okhremchuk <anastasiia.okhremchuk@gmail.com>
2026-05-08 20:42:03 +0300

    Feat: create player state

---
+++ Source/CovenHollow/Private/Core/MainPlayerState.cpp
@@ -0,0 +1,6 @@
+#include "Core/MainPlayerState.h"
+
+AMainPlayerState::AMainPlayerState()
+{
+	NetUpdateFrequency = 100.0f;
+}
---
+++ Source/CovenHollow/Public/Core/MainPlayerState.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+
+#include "MainPlayerState.generated.h"
+
+UCLASS()
+class COVENHOLLOW_API AMainPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AMainPlayerState();
+	
+};
