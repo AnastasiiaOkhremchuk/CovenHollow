e1b0de2279bce6a331458e3871060e5b2c42e2d6
Anastasiia Okhremchuk <anastasiia.okhremchuk@gmail.com>
2026-06-15 23:20:15 +0300

    Feat: get all asset tags

--- Source/CovenHollow/Private/AbilitySystem/BaseAbilitySystemComponent.cpp
+++ Source/CovenHollow/Private/AbilitySystem/BaseAbilitySystemComponent.cpp
@@ -1,11 +1,17 @@
 #include "AbilitySystem/BaseAbilitySystemComponent.h"
 
-void UBaseAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
-{
-	GEngine->AddOnScreenDebugMessage(1, 8.0f, FColor::Blue, FString("Effect applied"));
-}
-
 void UBaseAbilitySystemComponent::InitAbilityActorInfo()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UBaseAbilitySystemComponent::EffectApplied);
+}
+
+void UBaseAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
+{
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		const FString Msg = FString::Printf(TEXT("GE tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.0f, FColor::Blue, Msg);
+	}
 }
