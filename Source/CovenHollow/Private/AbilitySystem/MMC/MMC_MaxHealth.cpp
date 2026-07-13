8492e3ec1227c23571fbafb18ecc0f4c74b550ea
Anastasiia Okhremchuk <anastasiia.okhremchuk@gmail.com>
2026-07-13 23:19:38 +0300

    Feat: add max health magnitude calculation

---
+++ Source/CovenHollow/Private/AbilitySystem/MMC/MMC_MaxHealth.cpp
@@ -0,0 +1,31 @@
+#include "AbilitySystem/MMC/MMC_MaxHealth.h"
+#include "AbilitySystem/BaseAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxHealth::UMMC_MaxHealth()
+{
+	VigorDef.AttributeToCapture = UBaseAttributeSet::GetVigorAttribute();
+	VigorDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	VigorDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(VigorDef);
+}
+
+float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = SourceTags;
+	EvaluateParameters.TargetTags = TargetTags;
+
+	float Vigor = 0.0f;
+	GetCapturedAttributeMagnitude(VigorDef, Spec, EvaluateParameters, Vigor);
+	Vigor = FMath::Max<float>(Vigor, 0.0f);
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	int32 CharacterLevel = CombatInterface->GetCharacterLevel();
+
+	return 80.0f +2.5f * Vigor + 10.0f * CharacterLevel;
+}
